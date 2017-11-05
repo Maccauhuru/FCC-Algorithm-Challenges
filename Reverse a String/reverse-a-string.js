4bb907be056d00fb1606125fa55f5cd641efc99b
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-11-05 13:32:57 -0600

    initial commit to repository

---
+++ Reverse a String/reverse-a-string.js
@@ -0,0 +1,10 @@
+//Function reverses any string argument assigned to it
+
+function reverseString(str) {
+    //split the string
+    //use the reverse method
+    //join the string back together
+  return str.split("").reverse().join("");
+}
+
+reverseString("hello");
