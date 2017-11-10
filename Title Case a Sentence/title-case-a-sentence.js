2163033b668f6147006f3ae681171bbfe4240afc
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2017-11-09 22:38:05 -0600

    modified comments

--- Title Case a Sentence/title-case-a-sentence.js
+++ Title Case a Sentence/title-case-a-sentence.js
@@ -1,18 +1,18 @@
 
 function titleCase(str) {
-  //1.covert all the letters to lower case
+  //1.convert all the letters to lower case
   str = str.toLowerCase();
 
-  //2.split all of the letters
+  //2.split all the words in str by an empty space
   str = str.split(" ");
 
-  //3.loop through the provided string and capitalize the first character
+  //3.loop through the string and capitalize the first character of each word
   for (var i = 0; i < str.length; i++) {
-    //join the first character that you capitalize with the rest of the sliced string
+    //join the first character that you capitalize with the rest of the word
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
   
-  //return the joined strings after capitalization of their first chars
+  //return the joined strings after capitalization of their first characters
   return str.join(" ");
 }
 
